68b4d9a9e87a7c5c242a2ad3e716e01708d2788a
Jacob Halvorson <33240075+jhalvorson6687@users.noreply.github.com>
2021-01-09 00:37:37 -0800

    Update navbar.jsx
    New resume link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -57,7 +57,7 @@ class NavBar extends Component {
                 <li>
                   <a
                     class="nav-link navbar-link"
-                    href="https://drive.google.com/file/d/1h1xcyW5ds7yK_4WcFcR1bnPlooAZSW-T/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1HGxXQT7NNjmbR8hQ9MievbT4kOQw0h1s/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
